50f1e4d247952a48b31e9b72dbdd4f13dbe40941
NadiaGamula <nadia.zakharova@gmail.com>
2016-12-13 14:06:02 +0200

    Fix behavior when question was moved to other objective

--- app/studying/viewmodels/index.js
+++ app/studying/viewmodels/index.js
@@ -1,4 +1,5 @@
-﻿define(['knockout', 'durandal/app', 'durandal/activator', './studyadvice', './readings'], function (ko, app, activator, studyadvice, readings) {
+﻿define(['knockout', 'durandal/app', 'durandal/activator', './studyadvice', './readings', 'entities/course'],
+function (ko, app, activator, studyadvice, readings, course) {
     "use strict";
 
     var viewModel = {
@@ -20,15 +21,23 @@
     return viewModel;
 
     function activate(sectionId, questionId) {
-        if (sectionId && questionId) {
+        if (sectionId && questionId && isQuestionInSection(sectionId, questionId)) {
             viewModel.activeItem.activateItem(readings, [sectionId, questionId]);
         }
         else {
             return viewModel.activeItem.activateItem(studyadvice);
         }
-
-
     }
 
-
+    function isQuestionInSection(sectionId, questionId) {
+        var section = _.find(course.sections, function (section) { return section.id === sectionId; });
+        if (_.isNull(section) || _.isUndefined(section)) {
+            return false;
+        }
+        var question = _.find(section.questions, function (question) { return question.id === questionId; });
+        if (_.isNull(question) || _.isUndefined(question)) {
+            return false;
+        }
+        return true;
+    }
 });
